fdc19e2f88fb830f7ded0bb3cd49f68aa7e928aa
Daniel Schuller <danielsc@hq.unity3d.com>
2017-09-06 16:37:29 +0100

    Added a touch command and made open work on files too

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -20,21 +20,68 @@ function Reload-Profile {
     }    
 }
 
+function Touch-File
+{
+    $file = $args[0]
+    if($file -eq $null) {
+        throw "No filename supplied"
+    }
+
+    if(Test-Path $file)
+    {
+        (Get-ChildItem $file).LastWriteTime = Get-Date
+    }
+    else
+    {
+        echo $null > $file
+    }
+}
+
 # Gets the ip4 address, extracted from ipconfig
 function ip4 { (ipconfig)-like'*IPv4*'|%{($_-split': ')[-1]}}
-function open { ii @args }
+function open 
+{ 
+    if(Test-Path $args[0] -PathType Leaf)
+    {
+        Invoke-Item $args[0]
+    }
+    else
+    {
+        ii @args 
+    }
+}
 function code { Set-Location ~\Code }
 function serve
 {
-	echo "Address: http://$(ip4):8000/mercurial"
-	hg serve --prefix mercurial --address  $(ip4)
+    echo "Address: http://$(ip4):8000/mercurial"
+    hg serve --prefix mercurial --address  $(ip4)
 }
+
+function dinit
+{
+   echo "Initialising a mercurial checkout for a unity project" 
+}
+
+function tsubl
+{
+    touch @args
+    subl @args
+}
+
 Set-Alias refresh Reload-Profile
+Set-Alias touch Touch-File
 
 # Load posh-hg example profile
 if (Test-path 'C:\Users\danielsc\code\posh-hg\profile.example.ps1')
 {
-	. 'C:\Users\danielsc\code\posh-hg\profile.example.ps1'
+    . 'C:\Users\danielsc\code\posh-hg\profile.example.ps1'
 }
 
+# Jump in the code directory by default
 code
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
